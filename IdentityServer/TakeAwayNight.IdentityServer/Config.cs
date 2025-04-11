b70b57f19f9c05b09055c4a2b103475237cfdacb
Batuhan YALIN <batuhanyalin2@gmail.com>
2025-04-11 16:44:59 +0300

    IdentityServer Config.cs ApiResource IdentityResource ve Clientlar oluşturuldu.

--- IdentityServer/TakeAwayNight.IdentityServer/Config.cs
+++ IdentityServer/TakeAwayNight.IdentityServer/Config.cs
@@ -10,101 +10,70 @@ using static IdentityServer4.Models.IdentityResources;
 
 namespace TakeAwayNight.IdentityServer
 {
-    // Config sınıfı, bir IdentityServer instance'ı için çeşitli kaynakları ve ayarları yapılandırmak amacıyla kullanılır. IdentityServer, uygulamaları güvence altına almak için OpenID Connect ve OAuth 2.0 uygulamak için kullanılan açık kaynaklı bir çerçevedir. 
+
     public static class Config
     {
-        //ApiResource: İstemcilerin erişmek istediği bir kaynağı temsil eder.
-       // Scopes: Bu kaynağın sahip olabileceği kapsamları tanımlar.Kapsamlar, istemcinin ne yapabileceğini belirler.
-        public static IEnumerable<ApiResource> ApiResources =>
-                        new ApiResource[]
-                        {
-                new ApiResource("ResourceCatalog"){Scopes={"CatalogFullPermission","CatalogReadPermission"}},
-                new ApiResource("ResourceDiscount"){Scopes={"DiscountFullPermission"} },
-                new ApiResource("ResourceDiscount2"){Scopes={"DiscountDeletePermission"} },
-                new ApiResource("ResourceOrder"){Scopes={"OrderFullPermission"}},
-                 new ApiResource("ResourceCargo"){Scopes={"CargoFullPermission"} },
-                 new ApiResource("ResourceBasket"){Scopes={"BasketFullPermission"} },
-                 new ApiResource("ResourceComment"){Scopes={"CommentFullPermission"} },
-                 new ApiResource("ResourceOcelot"){Scopes={"OcelotFullPermission"} },
-                 new ApiResource("ResourceMessage"){Scopes={"MessageFullPermission"} },
-                new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
-     };
 
-        //Bu özellik, kimlik sağlayıcısından istenebilecek kullanıcı bilgilerini tanımlar.
-        //IdentityResources.OpenId(): Kullanıcı kimlik doğrulaması için OpenID Connect standardını temsil eder.
-        // IdentityResources.Email(): Kullanıcının e-posta adresini temsil eder.
-       // IdentityResources.Profile(): Kullanıcının profil bilgilerini temsil eder.
-        public static IEnumerable<IdentityResource> IdentityResources =>
-             new IdentityResource[]
-             {
-                new IdentityResources.OpenId(),
-                new IdentityResources.Email(),
-                new IdentityResources.Profile()
+        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+        {
+            //Burada yetkilerin dağıtımı hazırlanıyor
+            new ApiResource("ResourceCatalog"){ Scopes = { "CatalogFullPermission", "CatalogReadPermission" }},
+            new ApiResource("ResourceDiscount"){Scopes={"DiscountFullPermission"}},
+            new ApiResource("ResourceDiscount2"){Scopes={"DiscountDeletePermission"}},
+            new ApiResource("ResourceOrder"){Scopes={"OrderFullPermission"}},
+            new ApiResource("ResourceCargo"){Scopes={"CargoFullPermission"}},
+            new ApiResource("ResourceBasket"){Scopes={"BasketFullPermission"}},
+            new ApiResource("ResourceComment"){Scopes={"CommentFullPermission"}},
+            new ApiResource("ResourceOcelot"){Scopes={"OcelotFullPermission"}},
+            new ApiResource("ResourceMessage"){Scopes={"MessageFullPermission"}},
+            new ApiResource(IdentityServerConstants.LocalApi.ScopeName) //Apiresourcelara erişim sağlarken scopeların isimlerine göre çağırmayı sağlıyor.
+        };
 
-             };
-        //Bu özellik, sistemde mevcut olan kapsamları tanımlar. Kapsamlar, istemcinin ne yapmasına izin verildiğini belirtmek için kullanılır.
-        public static IEnumerable<ApiScope> ApiScopes =>
-                new ApiScope[]
-                {
-                new ApiScope("CatalogFullPermission","Full authority for catalog operations"),
-                new ApiScope("CatalogReadPermission","Reading authority for catalog operations"),
-                new ApiScope("DiscountFullPermission","Full authority for discount operations"),
-                new ApiScope("DiscountDeletePermission","Full authority for discount operations"),
-                new ApiScope("OrderFullPermission","Full authority for order operations"),
-                 new ApiScope("CargoFullPermission","Full authority for cargo operations"),
-                 new ApiScope("BasketFullPermission","Full authority for basket operations"),
-                 new ApiScope("CommentFullPermission","Full authority for comment operations"),
-                 new ApiScope("OcelotFullPermission","Full authority for ocelot operations"),
-                 new ApiScope("MessageFullPermission","Full authority for message operations"),
-                new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
-                };
+        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResources.Email(),
+        };
 
+        public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
+        {
+            new ApiScope("CatalogFullPermission","Full Authority For Catalog Operations"),
+            new ApiScope("DiscountFullPermission","Full Authority For Discount Operations"),
+            new ApiScope("DiscountDeletePermission","DiscountDelete Authority For Discount Delete"),
+            new ApiScope("OrderFullPermission","Full Authority For Order Operations"),
+            new ApiScope("CargoFullPermission","Full Authority For Cargo Operations"),
+            new ApiScope("BasketFullPermission","Full Authority For Basket Operations"),
+            new ApiScope("CommentFullPermission","Full Authority For Comment Operations"),
+            new ApiScope("OcelotFullPermission","Full Authority For Ocelot Operations"),
+            new ApiScope("MessageFullPermission","Full Authority For Message Operations"),
+            new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
+        };
         public static IEnumerable<Client> Clients => new Client[]
+        {
+            new Client
             {
-                // visitor
-                new Client
-                {
-                    ClientId = "TakeAwayVisitorId",
-                    ClientName = "Take Away Visitor User",
-                    //bu kullanıcı nasıl erişim sağlıyacak
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("takeawaysecret".Sha256()) },
 
-                    AllowedScopes = { "CatalogReadPermission", "OcelotFullPermission", IdentityServerConstants.LocalApi.ScopeName },
-                    AccessTokenLifetime=300
-                },
+                ClientId="TakeAwayVisitorId",
+                ClientName="TakeAwayVisitorUser",
+                AllowedGrantTypes=GrantTypes.ClientCredentials, // Kullanıcının nasıl erişim sağlayacağını belirtiyoruz
+                ClientSecrets={new Secret("takeawaysecret".Sha256())},  //Token alabilmesi için belirlenen key veriliyor.
+                AllowedScopes={"CatalogFullPermission","OcelotFullPermission",IdentityServerConstants.LocalApi.ScopeName}, //Yetkileri belirleniyor.
+                AccessTokenLifetime= 300
+             },
 
-                //Manager
-            //new Client
-            //{
-            //    ClientId="TakeAwayManagerId",
-            //    ClientName="Take Away Manager User",
-            //    AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
-            //    ClientSecrets={new Secret("multishopsecret".Sha256()) },
-            //    AllowedScopes={ "CatalogReadPermission", "CatalogFullPermission", "BasketFullPermission",
-            //    IdentityServerConstants.LocalApi.ScopeName,
-            //    IdentityServerConstants.StandardScopes.Email,
-            //    IdentityServerConstants.StandardScopes.OpenId,
-            //    IdentityServerConstants.StandardScopes.Profile }
-            //},
-
-            //Admin
             new Client
             {
                 ClientId="TakeAwayAdminId",
-                ClientName="Take Away Admin User",
-                AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
-                ClientSecrets={new Secret("takeawaysecret".Sha256()) },
-                AllowedScopes={ "CatalogFullPermission", "CatalogReadPermission", "DiscountFullPermission", "DiscountDeletePermission","OrderFullPermission"
-                ,"CargoFullPermission", "BasketFullPermission","CommentFullPermission","OcelotFullPermission","MessageFullPermission",IdentityServerConstants.LocalApi.ScopeName,
-                IdentityServerConstants.StandardScopes.Email,
-                IdentityServerConstants.StandardScopes.OpenId,
-                IdentityServerConstants.StandardScopes.Profile
-                },
-                AccessTokenLifetime=6000
+                ClientName="TakeAwayAdminUser",
+                AllowedGrantTypes=GrantTypes.ResourceOwnerPassword, //Bu clienta erişim sağlayabilmek için login olmak gerekecek.
+                ClientSecrets={new Secret("takeawaysecret".Sha256())},
+                AllowedScopes={ "CatalogFullPermission", "DiscountFullPermission","OrderFullPermission","CargoFullPermission","BasketFullPermission","CommentFullPermission","OcelotFullPermission","MessageFullPermission",IdentityServerConstants.LocalApi.ScopeName,
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Email,
+                    IdentityServerConstants.StandardScopes.Profile},
+                AccessTokenLifetime=750,
             }
-        };
-
-
+         };
     }
 }
